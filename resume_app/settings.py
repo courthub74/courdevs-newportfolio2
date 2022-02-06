dde895a868efbb3cc1a0df81a24968c4b95c23e8
Courdevelops <courdevelops@gmail.com>
2022-02-06 16:45:03 -0500

    djangoherokustaticfilesfalse

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -138,4 +138,4 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(),staticfiles=False)
